73d4f0728b2e23aa01ea56694b801ee701483d86
sudhakar <punjusudhakaryadav1724@gmail.com>
2024-10-08 20:27:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function jointoday(){
+    let name=prompt("What is your name?");
+    alert("Hello"+" "+name+" "+"More information coming soon");
+}
+
+document.querySelector('.auth-buttons').addEventListener('click',function(){
+    alert("More information coming soon")
+});
+document.querySelector('.main').addEventListener('click',function(){
+    alert("More information coming soon")
+});
+document.querySelector('.cta-button').addEventListener('click',function(){
+    alert("More information coming soon")
+});
